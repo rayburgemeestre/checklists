c74cc1b4e15ea65f55478b52e983aa0a8920f80d
Ray Burgemeestre <ray@cppse.nl>
2022-06-22 21:56:42 +0200

    Try todo example in Nuxt

--- pages/inspire.vue
+++ pages/inspire.vue
@@ -1,21 +1,72 @@
 <template>
-  <v-row>
-    <v-col class="text-center">
-      <img src="/v.png" alt="Vuetify.js" class="mb-5" />
-      <blockquote class="blockquote">
-        &#8220;First, solve the problem. Then, write the code.&#8221;
-        <footer>
-          <small>
-            <em>&mdash;John Johnson</em>
-          </small>
-        </footer>
-      </blockquote>
-    </v-col>
-  </v-row>
+  <div>
+    <input type="text" @keyup.enter="addTodo" placeholder="What needs to be done?">
+    <ul>
+      <li v-for="todo in todos" :key="todo.id">
+        <input :checked="todo.done" @change="toggle(todo)" type="checkbox" :id="todo.id">
+        <label :class="{ done: todo.done }" :for="todo.id">{{ todo.text }}</label>
+        <button @click="removeTodo(todo)">remove</button>
+      </li>
+    </ul>
+  </div>
 </template>
 
 <script>
+import { mapMutations } from 'vuex'
+
 export default {
   name: 'InspirePage',
+
+  computed: {
+    todos () {
+      return this.$store.state.todos.list
+    }
+  },
+  methods: {
+    addTodo (event) {
+      this.$store.commit('todos/add', event.target.value)
+      event.target.value = ''
+    },
+    ...mapMutations({
+      toggle: 'todos/toggle'
+    }),
+    removeTodo (todo){
+      this.$store.commit('todos/remove', todo)
+    }
+  }
 }
 </script>
+
+<style scoped>
+div {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+}
+
+ul {
+  display: inline-flex;
+  flex-direction: column;
+  align-items: flex-start;
+}
+
+li {
+  display: flex;
+  align-items: center;
+  margin-bottom: 0.5rem;
+}
+
+input[type="checkbox"] {
+  margin: 0.5rem;
+}
+
+button {
+  padding: 0.5rem 1rem;
+  font-size: 1rem;
+}
+
+.done {
+  text-decoration: line-through;
+}
+</style>
